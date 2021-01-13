896217787b117dedb7b6e47f8f39ebb87f58980f
Aromal Benny <aromalbenny17.com>
2021-01-13 12:53:23 +0530

    frontend initial commit

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import { SharedService } from './shared.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [SharedService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
